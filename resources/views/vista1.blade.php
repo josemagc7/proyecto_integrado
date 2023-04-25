feabe7344f49c1bf97bcb68b2fbf72946f9ca36c
josemagc7 <josemag.c.77@gmail.com>
2023-04-25 18:01:21 +0200

    todo

---
+++ resources/views/vista1.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Citas') }}
+        </h2>
+    </x-slot>
+    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-12 lg:px-12">
+            <div class="bg-black overflow-hidden shadow-xl sm:rounded-lg">
+                <h1>hola</h1>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
